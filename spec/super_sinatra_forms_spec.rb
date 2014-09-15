c6fef8e4168f566afa71d02387f60774c9a0a369
ahimmelstoss <ahimmelstoss@gmail.com>
2014-09-15 12:44:41 -0400

    tests

---
+++ spec/super_sinatra_forms_spec.rb
@@ -0,0 +1,53 @@
+describe App do
+
+  describe 'GET /' do
+    before do 
+      get '/'
+    end
+
+    it 'sends a 200 status code' do
+      expect(last_response.status).to eq(200)
+    end
+
+    it 'renders super hero form' do 
+      expect(last_response.body).to include("Create a Team and Heroes!")
+      expect(last_response.body).to include("<form")
+    end
+  end
+
+  describe 'POST /teams' do
+    before do
+      post '/team', {
+        :team => {
+          :name => "Team Ruby",
+          :motto => "We love Ruby!",
+          :members => {
+            :super1 => {
+              :name => "Amanda",
+              :power => "Ruby",
+              :bio => "I love Ruby!"  
+            },
+            :super2 => {
+              :name => "Arel",
+              :power => "JavaScript",
+              :bio => "I love JavaScript!"  
+            },
+            :super3 => {
+              :name => "Katie",
+              :power => "Sinatra",
+              :bio => "I love Sinatra!"  
+            }
+          }
+        }
+      }
+    end
+
+    it 'sends a 200 status code' do 
+      expect(last_response.status).to eq(200)
+    end
+
+    it 'displays the team info upon submission' do 
+      expect(last_response.body).to include("We love Ruby!")
+    end
+  end
+end
